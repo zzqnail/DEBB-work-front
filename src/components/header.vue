bcf2a053f12985a61c1495e35c6eeaa4b0361295
Argon0ut <nusupoversultan228@gmail.com>
2025-05-31 14:18:31 +0500

    header settings

---
+++ src/components/header.vue
@@ -0,0 +1,28 @@
+<script setup>
+
+</script>
+
+<template>
+    <div class="header">
+        <div class="headerLeft">
+            <button>Projects</button>
+            <div class="headerBorder"></div>
+        </div>
+
+
+    </div>
+        
+
+    
+
+</template>
+
+
+<style scoped>
+.headerBorder{
+    width: 1px;
+    background-color: #000;
+    margin-left: 10px;
+}
+
+</style>
---
+++ src/pages/MainPage.vue
@@ -0,0 +1,19 @@
+<script setup>
+import Header from '../components/header.vue'
+
+
+</script>
+
+
+<template>
+    <body>
+        <Header/>
+
+    </body>
+</template>
+
+
+<style scoped>
+
+</style>
+
