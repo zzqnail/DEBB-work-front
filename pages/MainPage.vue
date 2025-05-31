7d4d67cb85f8d192308a53777ce855f8c3850276
Argon0ut <nusupoversultan228@gmail.com>
2025-05-31 13:31:13 +0500

    adding router

---
+++ pages/MainPage.vue
@@ -0,0 +1,4 @@
+<template>
+    MAIN PAGE
+
+</template>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,19 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import {createRouter, createWebHistory} from 'vue-router'
+import MainPagevue from '../pages/MainPage.vue'
 
-createApp(App).mount('#app')
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes: [
+        {   path: '/', 
+            name: 'MainPage', 
+            component: MainPagevue 
+        }]
+
+})
+
+
+createApp(App).use(router).mount('#app')
